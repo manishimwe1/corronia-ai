a216c0c7a55200d9c2ba822d74faa34870022196
manishimwe1 <emmymanishimwem@gmail.com>
2024-06-12 17:05:05 +0200

    first commit

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+	publicRoutes: ["/", "/auth(.*)", "/portal(.*)"],
+	ignoredRoutes: ["/chatbot"],
+});
+
+export const config = {
+	matcher: [
+		"/((?!.+.[w]+$|_next).*)",
+		"/",
+		"/(api|trpc)(.*)",
+	],
+};
